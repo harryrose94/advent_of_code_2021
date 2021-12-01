2a2a82d1ea81ef7f553e9e64838ee605e7c6e52f
harryrose94 <harry.rose94@gmail.com>
2021-12-01 13:32:20 +0000

    problem two

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -13,17 +13,42 @@ fn load_from_file(file_path: &str) -> Vec<u32> {
 
     numbers
 }
-fn main() {
-    let filename: &str = "/Users/harryrose/Projects/advent_of_code_2021/day_one/data/input.txt";
-    let nums: Vec<u32> = load_from_file(filename);
 
+fn problem_one(nums: &[u32]) -> u32 {
     let mut num_increases: u32 = 0;
-    let mut prev: u32 = u32::MAX;
+    let mut prev: &u32 = &u32::MAX;
     for v in nums {
         if v > prev {
             num_increases += 1;
         }
         prev = v;
     }
-    println!("{}", num_increases);
+    num_increases
+}
+
+fn problem_two(nums: &[u32]) -> u32 {
+    if nums.len() <= 3 {
+        return 0;
+    }
+
+    let mut prev_sum = nums[0] + nums[1] + nums[2];
+    let mut num_increases: u32 = 0;
+    let mut curr_sum: u32;
+
+    for i in 3..nums.len() {
+        curr_sum = nums[i] + nums[i - 1] + nums[i - 2];
+        if curr_sum > prev_sum {
+            num_increases += 1;
+        }
+        prev_sum = curr_sum;
+    }
+    num_increases
+}
+
+fn main() {
+    let filename: &str = "/Users/harryrose/Projects/advent_of_code_2021/day_one/data/input.txt";
+    let nums: Vec<u32> = load_from_file(filename);
+
+    println!("{}", problem_one(&nums));
+    println!("{}", problem_two(&nums));
 }
