04c3f00f59c55ea1a5c7f685f4dbdd07efba288f
harryrose94 <harry.rose94@gmail.com>
2021-12-01 12:14:21 +0000

    remove tgt files

--- day_one/target/debug/day_one.d
+++
@@ -1,1 +0,0 @@
-/Users/harryrose/Projects/advent_of_code_2021/day_one/target/debug/day_one: /Users/harryrose/Projects/advent_of_code_2021/day_one/src/main.rs
--- day_one/target/debug/deps/day_one-770a2cd9d8797b13.d
+++
@@ -1,5 +0,0 @@
-/Users/harryrose/Projects/advent_of_code_2021/day_one/target/debug/deps/day_one-770a2cd9d8797b13: src/main.rs
-
-/Users/harryrose/Projects/advent_of_code_2021/day_one/target/debug/deps/day_one-770a2cd9d8797b13.d: src/main.rs
-
-src/main.rs:
